9d8ba52bb0d66e3d9834a37b7336560f50dbd7ad
shaneoh10 <shaneaghada@gmail.com>
2021-08-16 11:21:53 +0000

    feat: Add content and links to posts page

--- posts/views.py
+++ posts/views.py
@@ -4,4 +4,4 @@ from django.shortcuts import render
 def posts(request):
     """ View to return the posts page """
 
-    return render(request, 'posts/posts.html')
+    return render(request, 'posts/posts.html', {'list': range(10)})
