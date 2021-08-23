04fc7627df77d3027ec9b8db3f9c8f281f49d531
shaneoh10 <shaneaghada@gmail.com>
2021-08-23 11:06:35 +0000

    feat: Add comment section to post_detail template

--- posts/models.py
+++ posts/models.py
@@ -33,7 +33,7 @@ class Post(models.Model):
 
 
 class Comment(models.Model):
-    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, related_name='comments', on_delete=models.CASCADE)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     body = models.TextField(null=False)
     comment_date = models.DateTimeField(default=datetime.now)
