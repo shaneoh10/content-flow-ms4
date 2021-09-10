ff6aa578817afa66d0989f48e9c975edd15c20ee
shaneoh10 <shaneaghada@gmail.com>
2021-09-10 08:48:13 +0000

    feat: Add token checkout view and template

--- tokens/urls.py
+++ tokens/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.tokens, name='tokens')
+    path('', views.tokens, name='tokens'),
+    path('checkout', views.token_checkout, name='token_checkout'),
 ]
--- tokens/views.py
+++ tokens/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
 
+@login_required
 def tokens(request):
     """ View to return the buy tokens page """
 
     return render(request, 'tokens/tokens.html')
+
+
+@login_required
+def token_checkout(request):
+    """ View to return the token checkout page """
+
+    return render(request, 'tokens/token_checkout.html')
