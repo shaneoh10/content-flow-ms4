2dab15ad292a6c359fae8a87ed9d1b31b187cee9
shaneoh10 <shaneaghada@gmail.com>
2021-09-15 13:01:00 +0000

    feat: Add image field to UserProfile model and edit templates to suit

--- profiles/models.py
+++ profiles/models.py
@@ -5,6 +5,8 @@ from django.contrib.auth.models import User
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     bio = models.TextField(default="default bio")
+    image = models.ImageField(null=True, blank=True,
+                              upload_to="profile_images/")
 
     def __str__(self):
         return self.user.username
