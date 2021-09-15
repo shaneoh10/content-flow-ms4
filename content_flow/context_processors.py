543fad465390c8f782ee8822c75046b8330db671
shaneoh10 <shaneaghada@gmail.com>
2021-09-15 10:02:55 +0000

    reafactor: Order category context by name and edit category_menu template

--- content_flow/context_processors.py
+++ content_flow/context_processors.py
@@ -3,6 +3,7 @@ from posts.models import Category
 
 def all_categories(request):
     categories = Category.objects.all()
+    categories = categories.order_by('category_name')
     context = {
         'categories': categories
     }
