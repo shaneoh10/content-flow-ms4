590fc5e210b92a012fc133e74ced565e558b3e43
shaneoh10 <shaneaghada@gmail.com>
2021-09-18 08:56:31 +0000

    feat: Add send_reward to tokens app, functionality for users to send rewards

--- tokens/urls.py
+++ tokens/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.tokens, name='tokens'),
     path('checkout', views.token_checkout, name='token_checkout'),
+    path('send_reward/<str:receiver>', views.send_reward, name='send_reward'),
 ]
--- tokens/views.py
+++ tokens/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from django.contrib import messages
+from annoying.utils import HttpResponseReload
 
 
 @login_required
@@ -14,3 +17,22 @@ def token_checkout(request):
     """ View to return the token checkout page """
 
     return render(request, 'tokens/token_checkout.html')
+
+
+@login_required
+def send_reward(request, receiver):
+    sender = get_object_or_404(User, id=request.user.id)
+    receiver = get_object_or_404(User, username=receiver)
+    tokens = int(request.POST.get('reward'))
+
+    if sender.userprofile.tokens_balance >= tokens:
+        sender.userprofile.tokens_balance -= tokens
+        receiver.userprofile.tokens_balance += tokens
+        receiver.userprofile.tokens_score += tokens
+        sender.userprofile.save()
+        receiver.userprofile.save()
+        messages.success(request, f'You sent {tokens} tokens to"{receiver}"')
+    else:
+        messages.success(request, 'Failed to send reward, insufficent balance')
+
+    return HttpResponseReload(request)
