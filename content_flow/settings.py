250c158081b1db223954bc09554084eb416182a7
shaneoh10 <shaneaghada@gmail.com>
2021-09-22 10:48:31 +0000

    feat: Set up secret key and debug in settings.py

--- content_flow/settings.py
+++ content_flow/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-^jl0a^%el71p0*h$@_%231u14s^)w3@w61de2%@2t%egi=*24z'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['content-flow-ms4.herokuapp.com', 'localhost']
 
