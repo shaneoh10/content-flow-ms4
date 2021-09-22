483351f33a86e4a845628751bb8da66b1dc90c5f
shaneoh10 <shaneaghada@gmail.com>
2021-09-22 09:29:56 +0000

    feat: Set up postgres db, prepare for deployment

--- tokens/urls.py
+++ tokens/urls.py
@@ -3,7 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.tokens, name='tokens'),
-    path('checkout', views.token_checkout, name='token_checkout'),
     path('success', views.order_success, name='order_success'),
     path('cancel', views.order_cancel, name='order_cancel'),
     path('send_reward/<str:receiver>', views.send_reward, name='send_reward'),
