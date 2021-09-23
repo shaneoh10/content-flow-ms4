d75e25a57cf405e7938490d604e98f62792c3f36
shaneoh10 <shaneaghada@gmail.com>
2021-09-23 08:26:55 +0000

    style: Edit styling of post sort buttons

--- posts/static/posts/js/posts.js
+++ posts/static/posts/js/posts.js
@@ -1,10 +1,11 @@
 // https://www.javascripttutorial.net/javascript-bom/javascript-get-query-string/
+
 const urlParams = new URLSearchParams(location.search);
 $('document').ready(function() {
     if( urlParams.has('sort')) {
-        $('.top-btn').addClass('border');
+        $('.top-btn').addClass('border-primary text-primary');
     } else {
-        $('.new-btn').addClass('border');
+        $('.new-btn').addClass('border-primary text-primary');
     }
     if( urlParams.has('q')) {
         $('#no-posts').text('Please enter search criteria.');
