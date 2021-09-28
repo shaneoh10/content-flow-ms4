b4ad6f1c6dd96842ee47ad1c4f8615fad2853b90
shaneoh10 <shaneaghada@gmail.com>
2021-09-28 12:14:23 +0000

    feat: Create order model in tokens app

--- tokens/models.py
+++ tokens/models.py
@@ -1,3 +1,4 @@
+import uuid
 from django.db import models
 
 
@@ -10,3 +11,32 @@ class Product(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Order(models.Model):
+    order_number = models.CharField(max_length=32, null=False, editable=False)
+    card_name = models.CharField(max_length=50, null=False, blank=False)
+    username = models.CharField(max_length=150, null=False, blank=False)
+    email = models.EmailField(max_length=254, null=False, blank=False)
+    date = models.DateTimeField(auto_now_add=True)
+    tokens = models.IntegerField(null=False, blank=False, default=0)
+    order_total = models.DecimalField(max_digits=10, decimal_places=2,
+                                      null=False, default=0)
+
+    def _generate_order_number(self):
+        """
+        Generate random order number using UUID
+        """
+        return uuid.uuid4().hex.upper()
+
+    def save(self, *args, **kwargs):
+        """
+        Override original save method to set order number
+        if no order number is set
+        """
+        if not self.order_number:
+            self.order_number = self._generate_order_number()
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.order_number
