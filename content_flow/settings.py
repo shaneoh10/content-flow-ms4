8a358c295cb44ba27f5510ddc8c9f7630491b3e5
Shane O'Hanlon <70384009+shaneoh10@users.noreply.github.com>
2023-01-14 18:16:59 +0000

    fix: Update settings.py to allow debug

--- content_flow/settings.py
+++ content_flow/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'DEVELOPMENT' in os.environ
+DEBUG = 'DEBUG' in os.environ
 
 ALLOWED_HOSTS = ['localhost']
 
@@ -209,7 +209,7 @@ if 'USE_AWS' in os.environ:
 
 
 # Following settings only make sense on production and may break development environments.
-if not DEBUG:    
+if not 'DEVELOPMENT' in os.environ:    
     # Tell Django to copy statics to the `staticfiles` directory
     # in your application directory on Render.
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
